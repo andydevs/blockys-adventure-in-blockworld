71bd68202058aeb2240162c5b611735b29c9805e
Anshul Kharbanda <akanshul97@gmail.com>
2020-08-01 15:20:20 -0400

    Improvements to PlayerMotor and added Public Interface

--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -2,16 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMotor : MonoBehaviour
 {
+    // Motion Parameters
+    public float moveMaxVelocity = 20;
     public float moveForce = 10;
     public float jumpForce = 500;
 
-    float x;
+    // Motion Axes
+    float move;
     float jump;
-    bool jumped;
-    bool grounded;
+
+    // Spawn point
     Vector2 spawn;
+
+    // Components
     Rigidbody2D r2d;
 
     void Start()
@@ -23,30 +29,56 @@ public class PlayerMotor : MonoBehaviour
     void Update()
     {
         // Get x and jump axes
-        x = Input.GetAxis("Horizontal");
-        jump = Input.GetAxis("Jump");
-
-        // Check if grounded
-        Debug.DrawRay(transform.position, Vector2.down*1.1f, Color.white);
-        grounded = Physics2D.Raycast(transform.position, Vector2.down, 1.1f);
+        SetMoveAxis(Input.GetAxis("Horizontal"));
+        SetJumpAxis(Input.GetAxis("Jump"));
     }
 
     void FixedUpdate()
     {
-        // Move left/right
-        r2d.AddForce(Vector2.right * x * moveForce);
-
-        // Jump
-        if (jump > 0 && !jumped && grounded) {
-
-            // Check if grounded
-            r2d.AddForce(Vector2.up * jumpForce);
-            jumped = true;
+        // Only if Grounded
+        if (IsGrounded())
+        {
+            // Move left/right if below velocity
+            if (r2d.velocity.sqrMagnitude < moveMaxVelocity*moveMaxVelocity)
+            {
+                r2d.AddForce(Vector2.right * move * moveForce);
+            }
+            
+            // Jump
+            if (jump > 0)
+            {
+                r2d.AddForce(Vector2.up * jumpForce);
+            }
         }
-        if (jump == 0) { jumped = false; }
     }
 
-    public void Respawn() {
+    public void SetMoveAxis(float val)
+    {
+        move = val;
+    }
+
+    public void SetJumpAxis(float val)
+    {
+        jump = val;
+    }
+
+    public bool IsLeftGrounded()
+    {
+        return Physics2D.Raycast(transform.position + Vector3.left*0.45f, Vector2.down, 1.1f);
+    }
+
+    public bool IsRightGrounded()
+    {
+        return Physics2D.Raycast(transform.position + Vector3.right*0.45f, Vector2.down, 1.1f);
+    }
+
+    public bool IsGrounded()
+    {
+        return IsLeftGrounded() || IsRightGrounded();
+    }
+
+    public void Respawn() 
+    {
         transform.position = spawn;
     }
 }
