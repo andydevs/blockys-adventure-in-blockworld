bb42fb49c689cb840e40581d801ed8ec2b265ccc
Anshul Kharbanda <akanshul97@gmail.com>
2020-12-20 14:19:03 -0500

    Added controller support to UI

---
+++ Assets/Scripts/UIWindow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIWindow : MonoBehaviour
+{
+    public GameObject firstSelected;
+
+    public void OnEnable() {
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(firstSelected);
+    }
+}
