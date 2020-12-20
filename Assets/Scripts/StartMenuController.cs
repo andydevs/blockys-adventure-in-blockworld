226eeb9e1adb0e682d644d6a1697670901c56557
Anshul Kharbanda <akanshul97@gmail.com>
2020-12-20 13:58:43 -0500

    Hooked up Start Menu

---
+++ Assets/Scripts/StartMenuController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenuController : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("DemoScene");
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit Application...");
+        Application.Quit(0);
+    }
+}
