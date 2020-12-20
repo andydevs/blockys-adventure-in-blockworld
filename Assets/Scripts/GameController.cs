125707ec6cd380bff257a3d53c8277afa5721b9a
Anshul Kharbanda <akanshul97@gmail.com>
2020-12-19 19:15:18 -0500

    Created UIController that manages menus

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,32 +7,53 @@ public class GameController : MonoBehaviour
 {
     // Sub objects
     SpawnPoint spawnPoint;
+    UIController uiController;
 
     // Start is called before the first frame update
     void Start()
     {
         // Get sub objects
         spawnPoint = GetComponentInChildren<SpawnPoint>();
+        uiController = GetComponentInChildren<UIController>();
     }
 
     public void OnStart(InputValue val)
     {
-
+        if (IsPaused()) Resume();
+        else Pause();
     }
 
     public void BlockysDead()
     {
-        spawnPoint.SpawnABlocky();
+        uiController.OpenDead();
     }
 
     public void BlockyWon()
     {
-        spawnPoint.SpawnABlocky();
+        uiController.OpenWin();
+    }
+
+    public bool IsPaused()
+    {
+        return Time.timeScale < 0.5f;
+    }
+
+    public void Resume()
+    {
+        uiController.CloseAll();
+        Time.timeScale = 1.0f;
+    }
+
+    public void Pause()
+    {
+        uiController.OpenPause();
+        Time.timeScale = 0.0f;
     }
 
     public void Restart()
     {
-
+        uiController.CloseAll();
+        spawnPoint.SpawnABlocky();
     }
 
     public void Quit()
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public GameObject winMenu;
+    public GameObject dieMenu;
+
+    public void CloseAll()
+    {
+        pauseMenu.SetActive(false);
+        winMenu.SetActive(false);
+        dieMenu.SetActive(false);
+    }
+
+    public void OpenPause()
+    {
+        CloseAll();
+        pauseMenu.SetActive(true);
+    }
+
+    public void OpenWin()
+    {
+        CloseAll();
+        winMenu.SetActive(true);
+    }
+
+    public void OpenDead()
+    {
+        CloseAll();
+        dieMenu.SetActive(true);
+    }
+}
