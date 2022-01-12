5dfb6a6a830a7bd18469db6b5ff485995d7661b3
andydevs <akanshul97@gmail.com>
2022-01-12 00:38:34 -0500

    Create end menu

---
+++ Assets/Scripts/EndMenuController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts
+{
+    public class EndMenuController : MonoBehaviour
+    {
+        public void BackToStart()
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        public void Quit()
+        {
+            Debug.Log("Quit Application...");
+            Application.Quit(0);
+        }
+    }
+}
